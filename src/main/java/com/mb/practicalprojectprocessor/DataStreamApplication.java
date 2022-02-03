821ccd9c4d683a6134c3c0b96266bec0b2183597
mrclbrchr <50078346+mrclbrchr@users.noreply.github.com>
2022-02-03 08:42:12 +0100

    added scheduling

--- src/main/java/com/mb/practicalprojectprocessor/DataStreamApplication.java
+++ src/main/java/com/mb/practicalprojectprocessor/DataStreamApplication.java
@@ -6,13 +6,29 @@ import com.orbitz.consul.NotRegisteredException;
 import com.orbitz.consul.model.agent.ImmutableRegistration;
 import com.orbitz.consul.model.agent.Registration;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
 import java.util.HashMap;
 import java.util.Map;
 
-
+@Configuration
+@EnableScheduling
+@SpringBootApplication
 public class DataStreamApplication {
 
+    static Consul consulClient = Consul.builder().build();
+
+    static AgentClient agentClient = consulClient.agentClient();
+
+    static String serviceId = "dataprocessor-numbers";
+
     public static void main(String[] args) throws NotRegisteredException, InterruptedException, MqttException {
+
+        SpringApplication.run(DataStreamApplication.class, args);
 
         Map<String, String> serviceMetadata = new HashMap<>();
         serviceMetadata.put("host","localhost");
@@ -21,22 +37,22 @@ public class DataStreamApplication {
         serviceMetadata.put("type","stream");
         serviceMetadata.put("topic", "numbers");
 
-        Consul consulClient = Consul.builder().build();
-
-        AgentClient agentClient = consulClient.agentClient();
-
-        String serviceId = "datastream-numbers";
         Registration service = ImmutableRegistration.builder()
                 .id(serviceId)
                 .name("pp-library")
-                .port(8300)
-                .check(Registration.RegCheck.ttl(100000L))
+                .port(8302)
+                .check(Registration.RegCheck.ttl(5L))
                 .meta(serviceMetadata)
                 .build();
 
         agentClient.register(service);
         agentClient.pass(serviceId);
         DataStream dataStream = new DataStream();
+    }
+
+    @Scheduled(fixedRate = 3000)
+    public static void aliveSignal() throws NotRegisteredException {
+        agentClient.pass(serviceId);
     }
 
 }
--- src/main/java/com/mb/practicalprojectprocessor/MqttHandler.java
+++ src/main/java/com/mb/practicalprojectprocessor/MqttHandler.java
@@ -6,43 +6,33 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-import java.util.Random;
-
 public class MqttHandler {
 
-    static Random rand = new Random();
     static String topic        = "numbers";
     static int qos             = 0;
     static String broker       = "tcp://localhost:1883";
     static String clientId     = "datastream-numbers";
     static MemoryPersistence persistence = new MemoryPersistence();
-    static MqttClient sampleClient;
+    static MqttClient client;
 
-    public MqttHandler() throws InterruptedException {
-        connect();
+    public MqttHandler() {
     }
 
     public static void send(Integer number) throws MqttException {
-        int rndnumber = rand.nextInt(100);
-        String content      = Integer.toString(rndnumber);
+        String content      = Integer.toString(number);
         MqttMessage message = new MqttMessage(content.getBytes());
         message.setQos(qos);
-        sampleClient.publish(topic, message);
+        client.publish(topic, message);
     }
 
-    public static void connect() throws InterruptedException{
+    public static void connect(){
 
             try {
-                sampleClient = new MqttClient(broker, clientId, persistence);
+                client = new MqttClient(broker, clientId, persistence);
                 MqttConnectOptions connOpts = new MqttConnectOptions();
                 connOpts.setCleanSession(true);
-                sampleClient.connect(connOpts);
+                client.connect(connOpts);
             } catch(MqttException me) {
-                System.out.println("reason "+me.getReasonCode());
-                System.out.println("msg "+me.getMessage());
-                System.out.println("loc "+me.getLocalizedMessage());
-                System.out.println("cause "+me.getCause());
-                System.out.println("excep "+me);
                 me.printStackTrace();
             }
         }
